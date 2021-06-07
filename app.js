53ddce74bc09057d296413af2ae398d766be7ffb
Sajjita Bhattacharya <62219377+sajjitaaa@users.noreply.github.com>
2021-06-07 13:21:35 +0530

    Update app.js

--- app.js
+++ app.js
@@ -64,5 +64,5 @@ app.post("/failure", function (req, res) {
 });
 
 app.listen(process.env.PORT || 3000, function () {
-  console.log("server is running on port 3000");
+  console.log("server is running");
 });
