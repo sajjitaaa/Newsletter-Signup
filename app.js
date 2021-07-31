6abdce81ae743891adb1acd09f0c5aa89bd209bc
sajjitaaa <bsajjita@gmail.com>
2021-07-31 18:55:34 +0530

    edits

--- app.js
+++ app.js
@@ -41,15 +41,15 @@ app.post("/", function (req, res) {
   const url = "https://us6.api.mailchimp.com/3.0/lists/9ea0ae0bfd";
   const options = {
     method: "POST",
-    auth: "sajjitab:a17a4ea64956d9c320748b71675678ef-us6",
+    auth: process.env.auth,
   };
 
   const request = https.request(url, options, function (response) {
-    // if (response.statusCode === 200) {
-    res.sendFile(__dirname + "/success.html");
-    // } else {
-    // res.sendFile(__dirname + "/failure.html");
-    // }
+    if (response.statusCode === 200) {
+      res.sendFile(__dirname + "/success.html");
+    } else {
+      res.sendFile(__dirname + "/failure.html");
+    }
   });
 
   request.write(jsonDATA);
